2ba9f0c7c40c0ec2685d7c09df7f5b321debf946
Evgeny Rodin <Evgeny_Rodin@epam.com>
2017-03-09 17:58:37 +0300

    service updated

--- src/app/containers/todos/todos.service.js
+++ src/app/containers/todos/todos.service.js
@@ -2,12 +2,26 @@ export default class TodosService {
 	constructor($http) {
 		'ngInject;'
 		this.$http = $http;
+
+		this.URL = 'http://localhost:3004/todos';
+		this.ID = 4;
 	}
 
 	getTodos() {
-		return this.$http.get('http://localhost:3004/todos')
+		return this.$http.get(this.URL)
 			.then(todos => todos.data)
-			.then(todos => todos)
+			// .then(todos => todos)
 			.catch(err => Promise.reject(err));
+	}
+
+	addTodo(todo) {
+		this.$http.post(this.URL, {
+			...todo, active: true, id: this.ID++
+		});
+	}
+
+	removeTodo({ todo }) {
+		//TODO
+		// this.todos = this.todos.filter(t => t.id !== todo.id);
 	}
 }

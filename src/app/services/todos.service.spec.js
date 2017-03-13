9528131b611bfa2c1742160a19cf7ac929f52fa5
Evgeny Rodin <Evgeny_Rodin@epam.com>
2017-03-13 18:15:01 +0300

    added test env plug-ins :bug: fix

--- src/app/services/todos.service.spec.js
+++ src/app/services/todos.service.spec.js
@@ -1,5 +1,47 @@
-describe('example', function () {
-	it('has a dummy spec to test 2 + 2', function () {
-		expect(2+2).toEqual(4);
+describe('Todos factory', () => {
+	const todos = [
+		{
+			title: 'Make coffee 😅',
+			created_at: 1489181986308,
+			active: false,
+			description: "",
+			id: "HJcQp9lie"
+		},
+		{
+			title: 'Drink coffee 😋',
+			created_at: 1489181998933,
+			active: false,
+			description: "",
+			id: "HJw4a9eol"
+		},
+		{
+			title: 'Do it again! 💫',
+			created_at: 1489182011834,
+			active: true,
+			description: "Do it now!",
+			id: "BJVrpqxsx"
+		}
+	];
+
+	let TodosService;
+
+	beforeEach(angular.mock.module('todoListApp'));
+
+	beforeEach(inject((_TodosService_) => {
+		TodosService = _TodosService_;
+	}))
+
+	it('should exist', function () {
+		expect(TodosService).toBeDefined();
 	});
+
+	describe('.getAll()', () => {
+		it('should exist', function () {
+			expect(TodosService.getAll).toBeDefined();
+		});
+
+		it('should return a hard-coded list of todos', function () {
+			expect(TodosService.getAll()).toEqual(todos);
+		});
+	})
 });

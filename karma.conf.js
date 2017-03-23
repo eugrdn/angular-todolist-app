0ba97f95cbab17670ed6bc5c8d481e68ca7d360f
Eugene Rodin <eugene.rodin1@gmail.com>
2017-03-23 10:44:09 +0300

    updated karma config

--- karma.conf.js
+++ karma.conf.js
@@ -3,24 +3,12 @@ var webpackConfig = require('./webpack.config');
 module.exports = function (config) {
     config.set({
         basePath: './src',
-        frameworks: ['jasmine'],
+        frameworks: ['jasmine','requirejs'],
         files: [
             './public/bower_components/angular/angular.min.js',
             './public/bower_components/angular-route/angular-route.min.js',
             './public/bower_components/angular-mocks/angular-mocks.js',
-            './app/app.module.js',
-            './app/app.config.js',
-            './app/constants/filter.constants.js',
-            './app/store/todos.state.js',
-            './app/services/todos.service.js',
-            './app/containers/todo-detail/todo-detail.component.js',
-            './app/components/todo-filter/todo-filter.component.js',
-            './app/components/todo-counter/todo-counter.component.js',
-            './app/components/todo-add-form/todo-add-form.component.js',
-            './app/directives/ccpStop.directive.js',
-            './app/components/todo/todo.component.js',
-            './app/components/todo-search-form/todo-search-form.component.js',
-            './app/containers/todos/todos.component.js'
+            './app/app.module.js'
         ],
         exclude: [],
         preprocessors: {
@@ -31,7 +19,8 @@ module.exports = function (config) {
             'karma-webpack',
             'karma-chrome-launcher',
             'karma-sourcemap-loader',
-            'karma-spec-reporter'
+            'karma-spec-reporter',
+            'karma-requirejs'
         ],
         reporters: ['spec'],
         port: 9876,
